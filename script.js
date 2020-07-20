4428409fe444a20e9788b54c7d2fa9ef64617c70
Angel Alexander <angale22@gmail.com>
2020-07-20 17:33:37 -0500

    major organization, made everything one function

--- script.js
+++ script.js
@@ -1,135 +1,80 @@
 
+//generate button
 var generateBtn = document.querySelector("#generate");
 
 //password variables
-
-var passwordLength = " ";
-var passwordChar = false; 
-var passwordNum = false;
-var passwordLower = false;
-var passwordUpper = false;
+var passwordLength;
+var passwordChar; 
+var passwordNum;
+var passwordLower;
+var passwordUpper;
 
 //arrays
-
-var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
-var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var randomNumber = ["0123456789"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var randomNumber = "0123456789";
 var randomSymbol = "[ !#$%&'()*+,-./:;<=>?@[\]^_`{|}~]";
 
 //confirm length prompt
+function generateNewPassword()
+{
+  //confirm criteria
+  var validCriteria = false;
+  while(!validCriteria)
+  {
+    confirmLower = confirm("Click OK to confirm including lowercase characters");
+    confirmUpper = confirm("Click OK to confirm including uppercase characters");   
+    confirmNum = confirm("Click OK to confirm including numeric characters");    
+    confirmChar = confirm("Click OK to confirm including special characters");
 
-function userLength() {
+    validCriteria = confirmChar || confirmNum ||  confirmLower || confirmUpper;
+
+    if(!validCriteria)
+      alert("Please select at least one option...");
+  }
+
+  // confirm length
   var howMany = prompt("How many characters would you like your password to contain? (8 - 128");
-
-while (isNaN(howMany) || howMany < 8 || howMany > 128)  {
-    alert("Please enter a valid number.")
+  while (isNaN(howMany) || howMany < 8 || howMany > 128)
+  {
+      alert("Please enter a valid number.");
   }
 
-//confirms
+  // create array of valid characters
+  var validCharacterArray = "";
 
-confirmChar = confirm("Click OK to confirm including special characters");
-confirmNum = confirm("Click OK to confirm including numeric characters");    
-confirmLower = confirm("Click OK to confirm including owercase characters");
-confirmUpper = confirm("Click OK to confirm including uppercase characters");   
-
-console.log(confirmChar);
-console.log(confirmNum);
-console.log(confirmLower);
-console.log(confirmUpper);
-
-//user input
-
-if (confirmChar == true) {
-  passwordChar = true
-}
-
-if (confirmNum == true) {
-  passwordNum = true
-}
-
-if (confirmLower == true) {
-  passwordLower = true
-}
-
-if (confirmUpper == true) {
-  passwordUpper = true
-}
-
-var randomFunction = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-function generatePassword(passwordChar, passwordNum, passwordLower, passwordUpper, passwordLength) {
-  
-  var generatedPassword = " ";
-
-  var confirmCount = (lower + upper + number + symbol)
-
-  var confirmArray = [{confirmChar}, {confirmNum}, {confirmLower}, {confirmUpper}];
-}
-
-console.log("confirmArray: ", confirmArray);
-
-//Object of random gen functions
-
-var randomFunction = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
-
-// Random gen functions
-
-function getRandomLower() {
-  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
-}
-
-console.log(getRandomLower());
-
-function getRandomUpper() {
-  return upperCase[Math.floor(Math.random() * upperCase.length)];
-}
-
-console.log(getRandomUpper());
-
-function getRandomNumber() {
-  return randomNumber[Math.floor(Math.random() * randomNumber.length)];
-}
-
-console.log(getRandomNumber());
-
-function getRandomSymbol() {
-  return randomSymbol[Math.floor(Math.random() * randomSymbol.length)];
-}
-
-console.log(getRandomSymbol());
-/*
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-*/ 
-
-// Write password to the #password input
-function writePassword(lower, upper, number, symbol) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  console.log(password)
+  if(confirmLower)
+  {
+    validCharacterArray += lowerCase;
   }
+  if(confirmUpper)
+  {
+    validCharacterArray += upperCase;
+  }
+  if(confirmNum)
+  {
+    validCharacterArray += randomNumber;
+  }
+  if(confirmChar)
+  {
+    validCharacterArray += randomSymbol;
+  }
+
+  // creates the password
+  // loop through number of characters in the password to create the password
+  var password = "";
+
+  for(i = 0; i < howMany; i++)
+  {
+    var randomCharacter = validCharacterArray.charAt(Math.floor(Math.random() * validCharacterArray.length));
+    password += randomCharacter;
+
+  }
+
+  // password appears in box
+  var passwordText = document.querySelector("#password");  
   passwordText.value = password;
-
 }
-
-return password;
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generateNewPassword);
